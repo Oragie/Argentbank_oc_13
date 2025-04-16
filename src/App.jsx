677d504b42326da7c1d0e9129067d367fcd51b24
Vinko <vinkoloic@gmail.com>
2025-04-16 09:54:19 +0200

    Implement Signin page with form handling and styling

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,16 @@
-import './utils/style/global.scss'
-import Home from "./pages/Home"
-import Signin from "./pages/Signin"
-import Dashboard from "./pages/Dashboard"
-import Signup from "./pages/Signup"
-import Error from "./components/404"
-import Header from "./components/Header"
-import Footer from "./components/Footer"
-import Logout from './components/Logout'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import "./utils/style/global.scss";
+import Home from "./pages/Home";
+import Signin from "./pages/Signin";
+import Dashboard from "./pages/Dashboard";
+import Signup from "./pages/Signup";
+import Error from "./components/404";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Logout from "./components/Logout";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import ProtectedRoute from "./components/ProtectedRoute";
 
 function App() {
-
   return (
     <>
       <Router>
@@ -23,18 +22,19 @@ function App() {
           <Route path="/logout" element={<Logout />} />
           <Route path="/signup" element={<Signup />} />
 
-
-          <Route path="/profile" element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          } />
-
+          <Route
+            path="/profile"
+            element={
+              <ProtectedRoute>
+                <Dashboard />
+              </ProtectedRoute>
+            }
+          />
         </Routes>
         <Footer />
       </Router>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/Signin/index.jsx
@@ -0,0 +1,89 @@
+import "./signin.scss";
+import { useNavigate } from "react-router-dom";
+import { useLoginAndGetUser } from "../../hooks/useLogin";
+import { useSelector } from "react-redux";
+import { selectLoadingSession } from "../../app/selectors";
+import LoaderSession from "../../components/Loader";
+
+function Signin() {
+  const navigate = useNavigate();
+  const { handleLogin, error } = useLoginAndGetUser();
+  const loading = useSelector(selectLoadingSession); // Récupérer l'état 'loading' depuis le store
+  const savedEmail = localStorage.getItem("email");
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+
+    // Récupérer les valeurs directement du formulaire
+    const form = e.target;
+    const email = form.email.value;
+    const password = form.password.value;
+    const rememberMe = form.rememberMe.checked;
+
+    // Appel de la fonction handleLogin
+    handleLogin(email, password);
+
+    // Gérer la case à cocher "Remember me"
+    // Si la case est cochée, sauvegarder l'email dans le localStorage
+    // Sinon, le supprimer
+    if (rememberMe) {
+      localStorage.setItem("email", email);
+    } else {
+      localStorage.removeItem("email");
+    }
+  };
+
+  return (
+    <div className="bodyPage">
+      <main className="main bg-dark">
+        <section className="sign-in-content">
+          <i className="fa fa-user-circle sign-in-icon"></i>
+          <h1>Sign In</h1>
+          <form onSubmit={handleSubmit}>
+            <div className="input-wrapper">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                required
+                defaultValue={savedEmail || ""}
+              />
+            </div>
+            <div className="input-wrapper">
+              <label htmlFor="password">Password</label>
+              <input type="password" id="password" name="password" required />
+            </div>
+            <div className="input-remember">
+              <input
+                type="checkbox"
+                id="remember-me"
+                name="rememberMe"
+                defaultChecked={savedEmail ? true : false} // Si un email est sauvegardé, la case sera cochée
+              />
+              <label htmlFor="remember-me">Remember me</label>
+            </div>
+            {/* Afficher le message d'erreur */}
+            {error && <p className="error-message">{error}</p>}
+            <button type="submit" className="sign-in-button" disabled={loading}>
+              {loading ? <LoaderSession /> : "Sign In"}
+            </button>
+          </form>
+        </section>
+        <p>Email : tony@stark.com / steve@rogers.com</p>
+        <p>Password : password123 / password456</p>
+        <section className="sign-in-content sign-up-content">
+          <h1>New customer</h1>
+          <button
+            onClick={() => navigate("/signup")}
+            className="sign-in-button"
+          >
+            Sign Up
+          </button>
+        </section>
+      </main>
+    </div>
+  );
+}
+
+export default Signin;
