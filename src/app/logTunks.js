18744984bc2a17309d5cb54266f539442380e2c8
Vinko <vinkoloic@gmail.com>
2025-04-17 11:47:18 +0200

    refactor: Standardize import statements and improve code formatting in thunk files

--- src/app/logTunks.js
+++ src/app/logTunks.js
@@ -1,15 +1,15 @@
-import { createAsyncThunk } from "@reduxjs/toolkit"
-import { login } from "../api/apiService"
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { login } from "./../api/apiService.js";
 
 // Création du thunk pour gérer la connexion utilisateur
 export const loginUser = createAsyncThunk(
-    'login/loginUser', 
-    async ({ email, password }, { rejectWithValue }) => {
-      try {
-        const response = await login(email, password);  // Appel à l'API
-        return response.body.token;  // Retourne le token de la réponse
-      } catch (error) {
-        return rejectWithValue(error.message || "Erreur de connexion API")
-      }
+  "login/loginUser",
+  async ({ email, password }, { rejectWithValue }) => {
+    try {
+      const response = await login(email, password); // Appel à l'API
+      return response.body.token; // Retourne le token de la réponse
+    } catch (error) {
+      return rejectWithValue(error.message || "Erreur de connexion API");
     }
-  )
+  }
+);
--- src/app/userThunks.js
+++ src/app/userThunks.js
@@ -1,27 +1,27 @@
-import { createAsyncThunk } from "@reduxjs/toolkit"
-import { updateUserProfile, getUserProfile } from "../api/apiService"
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import { updateUserProfile, getUserProfile } from "./../api/apiService.js";
 
 export const updateUser = createAsyncThunk(
   "user/updateUser",
   async (updatedData, { rejectWithValue }) => {
     try {
-      const response = await updateUserProfile(updatedData)
-      return response.body
+      const response = await updateUserProfile(updatedData);
+      return response.body;
     } catch (error) {
-      return rejectWithValue(error.message)
+      return rejectWithValue(error.message);
     }
   }
-)
+);
 
 // Thunk pour récupérer le profil utilisateur
 export const fetchUserProfile = createAsyncThunk(
-    "user/fetchUserProfile",
-    async (_, { rejectWithValue }) => {
-        try {
-            const response = await getUserProfile()
-            return response.body // Renvoie le profil utilisateur
-        } catch (error) {
-            return rejectWithValue(error.message) // Envoie un message d'erreur si l'API échoue
-        }
+  "user/fetchUserProfile",
+  async (_, { rejectWithValue }) => {
+    try {
+      const response = await getUserProfile();
+      return response.body; // Renvoie le profil utilisateur
+    } catch (error) {
+      return rejectWithValue(error.message); // Envoie un message d'erreur si l'API échoue
     }
-)
+  }
+);
