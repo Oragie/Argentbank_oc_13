45fdd7743e005b7388fc94e9aa2996e52528ab62
Vinko <vinkoloic@gmail.com>
2025-04-17 11:13:59 +0200

    feat: Implement API services for user authentication and data fetching; add feature data and mock account/transaction data; update SCSS imports and styles across components

--- src/app/selectors.js
+++ src/app/selectors.js
@@ -2,6 +2,6 @@
 
 export const selectUser = (state) => state.user; // Récupère l'objet complet de l'utilisateur
 export const selectFirstName = (state) => state.user.firstName; // Récupère le firstName de l'utilisateur
-// export const selectLastName = (state) => state.user.lastName; // Récupère le lastName de l'utilisateur
+export const selectLastName = (state) => state.user.lastName; // Récupère le lastName de l'utilisateur
 export const selectToken = (state) => state.login.token;
-export const selectLodingSession = (state) => state.login.loading;
+export const selectLoadingSession = (state) => state.login.loading;
